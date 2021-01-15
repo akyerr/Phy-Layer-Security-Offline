20bc7c4decfe1af1df45fa09d29cadc302c8190c
Anil Kumar Yerrapragada <anil@anil.local>
2021-01-14 23:08:01 -0600

    KER vs BER in FDomain

---
+++ TDomain/SynchSignal.py
@@ -0,0 +1,84 @@
+from numpy import sum, zeros, array, exp, pi, concatenate, conj, sqrt
+from numpy.fft import ifft
+import matplotlib.pyplot as plt
+
+
+class SynchSignal:
+    def __init__(self, pls_params, num_synch_symb, num_data_symb, symb_pattern):
+        self.bandwidth = pls_params.bandwidth
+        self.bin_spacing = pls_params.bin_spacing
+        self.num_ant = pls_params.num_ant
+        self.bit_codebook = pls_params.bit_codebook
+        self.synch_data_pattern = pls_params.synch_data_pattern
+        self.num_synch_symb = num_synch_symb
+        self.NFFT = pls_params.NFFT
+        self.CP = pls_params.CP
+        self.OFDMsymb_len = self.NFFT + self.CP
+        self.symb_pattern = symb_pattern
+        self.num_synch_bins = self.NFFT - 2
+        # self.used_synch_bins
+
+        DC_index = int(self.NFFT / 2)
+        neg_synch_bins = list(range(DC_index - int(self.num_synch_bins / 2), DC_index))
+        pos_synch_bins = list(range(DC_index + 1, DC_index + int(self.num_synch_bins / 2) + 1))
+        self.used_synch_bins = array(neg_synch_bins + pos_synch_bins)
+
+        self.num_data_symb = num_data_symb
+        self.prime_no = [23, 41]
+        self.prime_nos = self.prime_no * self.num_data_symb
+        self.num_unique_synch = len(self.prime_no)
+        assert len(self.prime_nos) == self.num_synch_symb
+
+        self.synch_signals = zeros((self.num_synch_symb, self.OFDMsymb_len), dtype=complex)
+
+        for symb in range(self.num_synch_symb):
+            synch_symb = self.zadoff_chu_gen(self.prime_nos[symb])  # size NFFT - 2
+
+            synch_freq = zeros(self.NFFT, dtype=complex)
+            synch_freq[self.used_synch_bins] = synch_symb # size NFFT
+
+            synch_ifft = ifft(synch_freq)  # size NFFT
+            synch_cp = synch_ifft[-self.CP:] # size CP
+            synch_time = concatenate((synch_cp, synch_ifft))  # size NFFT + CP
+
+            power_est = sum(synch_time*conj(synch_time))/len(synch_time)
+            norm_synch = synch_time/sqrt(power_est)
+
+            self.synch_signals[symb, :] = norm_synch
+
+        self.synch_mask = zeros((self.num_ant, self.OFDMsymb_len*len(self.symb_pattern)), dtype=complex)
+
+        total_symb_count = 0
+        synch_symb_count = 0
+        self.synch_start = list()
+        for symb in self.symb_pattern.tolist():
+            if symb == 0:
+                modulo_switch = int(synch_symb_count % int(self.num_ant * self.num_unique_synch))
+                symb_start = total_symb_count * self.OFDMsymb_len
+                symb_end = symb_start + self.OFDMsymb_len
+                # print(symb_start, symb_end)
+                self.synch_start.append(symb_start)
+                if modulo_switch == 0 or modulo_switch == 1:
+                    self.synch_mask[0, symb_start: symb_end] = self.synch_signals[synch_symb_count]
+                elif modulo_switch == 2 or modulo_switch == 3:
+                    self.synch_mask[1, symb_start: symb_end] = self.synch_signals[synch_symb_count]
+                synch_symb_count += 1
+
+            total_symb_count += 1
+        # print(self.synch_start)
+        plt.plot(self.synch_mask[0, :].real)
+        plt.plot(self.synch_mask[0, :].imag)
+        plt.show()
+        plt.plot(self.synch_mask[1, :].real)
+        plt.plot(self.synch_mask[1, :].imag)
+        plt.show()
+
+    def zadoff_chu_gen(self, prime):
+        x0 = array(range(0, self.num_synch_bins))
+        x1 = array(range(1, self.num_synch_bins + 1))
+        if self.num_synch_bins % 2 == 0:
+            zadoff_chu = exp(-1j * (2 * pi / self.num_synch_bins) * prime * (x0**2 / 2))
+        else:
+            zadoff_chu = exp(-1j * (2 * pi / self.num_synch_bins) * prime * (x0 * x1) / 2)
+
+        return zadoff_chu
---
+++ TDomain/pls_script.py
@@ -0,0 +1,74 @@
+from numpy import ceil, tile, concatenate, zeros, ones, reshape, prod, bitwise_xor
+from numpy.random import randint
+# from TDomain.PLSParameters import PLSParameters
+# from TDomain.PLSTransmitter import PLSTransmitter
+
+from PLSParameters import PLSParameters
+from PLSTransmitter import PLSTransmitter
+from PLSReceiver import PLSReceiver
+from SynchSignal import SynchSignal
+
+SNRdB = 1000
+SNR_type = 'Analog'
+
+pls_profiles = {
+    0: {'bandwidth': 960e3,
+        'bin_spacing': 15e3,
+        'num_ant': 2,
+        'bit_codebook': 1,
+        'synch_data_pattern': [2, 1]},
+    # 1:{'bandwidth': 960e3,
+    #    'bin_spacing': 15e3,
+    #    'num_ant': 2,
+    #    'bit_codebook': 2,
+    #    'synch_data_pattern': [4, 2]},
+}
+
+pvt_info_len = 8  # bits
+
+for prof in pls_profiles.values():
+    pls_params = PLSParameters(prof)
+    # print(pls_params.num_subbands)
+    # if length of pvt info is not a perfect multiple of num sub-bands*bit codebook then append some bits to make it
+    # a multiple
+    num_data_symb = int(ceil(pvt_info_len / (pls_params.num_subbands * pls_params.bit_codebook)))
+    # 2, 1, 2, 1, 2, 1, 2, 1, 2, 1
+    num_synch_symb = pls_params.synch_data_pattern[0] * num_data_symb
+
+    total_num_symb = num_synch_symb + num_data_symb
+    num_synchdata_patterns = int(total_num_symb / sum(pls_params.synch_data_pattern))
+
+    symb_pattern0 = concatenate((zeros(pls_params.synch_data_pattern[0]), ones(pls_params.synch_data_pattern[1])))
+    symb_pattern = tile(symb_pattern0, num_synchdata_patterns)
+
+
+    # Generate synch
+    synch = SynchSignal(pls_params, num_synch_symb, num_data_symb, symb_pattern)
+
+    pls_tx = PLSTransmitter(pls_params, synch, symb_pattern, total_num_symb, num_data_symb, num_synch_symb)
+    pls_rx = PLSReceiver(pls_params, synch, symb_pattern, total_num_symb, num_data_symb, num_synch_symb, SNRdB, SNR_type)
+
+    # 1. Alice to Bob first transmission
+    buffer_tx_time_A, ref_sig_A, precoders_A = pls_tx.transmit_signal_gen('Alice0', num_data_symb)
+
+    # 1. Bob first reception
+    lsv_B0, rsv_B0, _ = pls_rx.receive_sig_process(buffer_tx_time_A, ref_sig_A, precoders_A)
+
+    # 2. Bob to Alice - pvt info transfer starts here
+    # pvt_info_bits_B = randint(0, 2, pvt_info_len)  # private info bits
+    # pvt_info_bits_B = ones(pvt_info_len)
+    pvt_info_bits_B = [0, 0, 0, 1, 1, 0, 1, 1]
+    # print(pvt_info_bits_B)
+    buffer_tx_time_B, ref_sig_B, precoders_B = pls_tx.transmit_signal_gen('Bob', num_data_symb, pvt_info_bits_B, lsv_B0)
+
+    #2. Alice reception
+    lsv_A, rsv_A, obs_info_bits_A = pls_rx.receive_sig_process(buffer_tx_time_B, ref_sig_B, precoders_B)
+
+    B_key_obs_at_A = concatenate(reshape(obs_info_bits_A, prod(obs_info_bits_A.shape)))
+    num_bit_err = bitwise_xor(B_key_obs_at_A, pvt_info_bits_B).sum()
+    print(f'Number of bit errors Bob to Alice: {num_bit_err}')
+    print('B Key OBS:', B_key_obs_at_A)
+
+
+
+
